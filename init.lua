4c6a68eccba1d0735858a79af4ba0e577e17e78c
Tobi L <tobias.leitz@gmx.de>
2024-11-03 17:33:55 +0100

    letzte init nach Urlaub

--- init.lua
+++ init.lua
@@ -8,150 +8,192 @@ vim.cmd("set shiftwidth=2")
 
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not vim.loop.fs_stat(lazypath) then
-  vim.fn.system({
-    "git",
-    "clone",
-    "--filter=blob:none",
-    "https://github.com/folke/lazy.nvim.git",
-    "--branch=stable", -- latest stable release
-    lazypath,
-  })
+	vim.fn.system({
+		"git",
+		"clone",
+		"--filter=blob:none",
+		"https://github.com/folke/lazy.nvim.git",
+		"--branch=stable", -- latest stable release
+		lazypath,
+	})
 end
 
 vim.opt.rtp:prepend(lazypath)
 
 local plugins = {
-  {
-    "catppuccin/nvim",
-    name = "catppuccin",
-    priority = 1000,
-  },
-  {
-    "nvim-telescope/telescope.nvim",
-    tag = "0.1.5",
-    dependencies = { "nvim-lua/penary.nvim" },
-  },
-  {
-    "nvim-telescope/telescope-ui-select.nvim",
-  },
-  {
-    "nvim-treesitter/nvim-treesitter",
-    build = ":TSUpdate",
-  },
-  {
-    "nvim-neo-tree/neo-tree.nvim",
-    branch = "v3.x",
-    dependencies = {
-      "nvim-lua/plenary.nvim",
-      "nvim-tree/nvim-web-devicons",
-      "MunifZanjim/nvim.nvim",
-    },
-  },
-  {
-    "nvim-lualine/lualine.nvim",
-  },
-  {
-    {
-      "VonHeikemen/lsp-zero.nvim",
-      branch = "v4.x",
-      lazy = true,
-      config = false,
-    },
-    {
-      "williamboman/mason.nvim",
-      lazy = false,
-      config = true,
-    },
+	{
+		"catppuccin/nvim",
+		name = "catppuccin",
+		priority = 1000,
+	},
+	{
+		"nvim-telescope/telescope.nvim",
+		tag = "0.1.5",
+		dependencies = { "nvim-lua/plenary.nvim" },
+	},
+	{
+		"nvim-telescope/telescope-ui-select.nvim",
+	},
+	{
+		"nvim-treesitter/nvim-treesitter",
+		build = ":TSUpdate",
+	},
+	{
+		"nvim-neo-tree/neo-tree.nvim",
+		branch = "v3.x",
+		dependencies = {
+			"nvim-lua/plenary.nvim",
+			"nvim-tree/nvim-web-devicons",
+		},
+	},
+	{
+		"nvim-lualine/lualine.nvim",
+		opts = {
+			theme = "dracula",
+		},
+	},
+	{
+		"VonHeikemen/lsp-zero.nvim",
+		branch = "v4.x",
+		lazy = true,
+		config = false,
+	},
+	{
+		"williamboman/mason.nvim",
+		lazy = false,
+		config = true,
+	},
+	{
+		"hrsh7th/nvim-cmp",
+		event = "InsertEnter",
+		dependencies = {
+			{ "L3MON4D3/LuaSnip" },
+		},
+		config = function()
+			local cmp = require("cmp")
+			cmp.setup({
+				sources = {
+					{ name = "nvim_lsp" },
+				},
+				mapping = cmp.mapping.preset.insert({
+					["<C-Space>"] = cmp.mapping.complete(),
+					["<C-u>"] = cmp.mapping.scroll_docs(-4),
+					["<C-d>"] = cmp.mapping.scroll_docs(4),
+				}),
+				snippet = {
+					expand = function(args)
+						require("luasnip").expand(args.body)
+					end,
+				},
+			})
+		end,
+	},
+	{
+		"neovim/nvim-lspconfig",
+		cmd = { "LspInfo", "LspInstall", "LspStart" },
+		event = { "BufReadPre", "BufNewFile" },
+		dependencies = {
+			{ "hrsh7th/cmp-nvim-lsp" },
+			{ "williamboman/mason-lspconfig.nvim" },
+		},
+		config = function()
+			local lsp_zero = require("lsp-zero")
 
-    -- Autocompletion
-    {
-      "hrsh7th/nvim-cmp",
-      event = "InsertEnter",
-      dependencies = {
-        { "L3MON4D3/LuaSnip" },
-      },
-      config = function()
-        local cmp = require("cmp")
+			local lsp_attach = function(client, bufnr)
+				local opts = { buffer = bufnr }
+				vim.keymap.set("n", "K", "<cmd>lua vim.lsp.buf.hover()<CR>", opts)
+				vim.keymap.set("n", "gd", "<cmd>lua vim.lsp.buf.definition()<CR>", opts)
+				vim.keymap.set("n", "gD", "<cmd>lua vim.lsp.buf.declaration()<CR>", opts)
+				vim.keymap.set("n", "gi", "<cmd>lua vim.lsp.buf.implementation()<CR>", opts)
+				vim.keymap.set("n", "go", "<cmd>lua vim.lsp.buf.type_definition()<CR>", opts)
+				vim.keymap.set("n", "gr", "<cmd>lua vim.lsp.buf.references()<CR>", opts)
+				vim.keymap.set("n", "gs", "<cmd>lua vim.lsp.buf.signature_help()<CR>", opts)
+				vim.keymap.set("n", "<F2>", "<cmd>lua vim.lsp.buf.rename()<CR>", opts)
+				vim.keymap.set({ "n", "x" }, "<F3>", "<cmd>lua vim.lsp.buf.format({async = true})<CR>", opts)
+				vim.keymap.set("n", "<F4>", "<cmd>lua vim.lsp.buf.code_action()<CR>", opts)
+			end
 
-        cmp.setup({
-          sources = {
-            { name = "nvim_lsp" },
-          },
-          mapping = cmp.mapping.preset.insert({
-            ["<C-Space>"] = cmp.mapping.complete(),
-            ["<C-u>"] = cmp.mapping.scroll_docs(-4),
-            ["<C-d>"] = cmp.mapping.scroll_docs(4),
-          }),
-          snippet = {
-            expand = function(args)
-              vim.snippet.expand(args.body)
-            end,
-          },
-        })
-      end,
-    },
+			lsp_zero.extend_lspconfig({
+				sign_text = true,
+				lsp_attach = lsp_attach,
+				capabilities = require("cmp_nvim_lsp").default_capabilities(),
+			})
 
-    -- LSP
-    {
-      "neovim/nvim-lspconfig",
-      cmd = { "LspInfo", "LspInstall", "LspStart" },
-      event = { "BufReadPre", "BufNewFile" },
-      dependencies = {
-        { "hrsh7th/cmp-nvim-lsp" },
-        { "williamboman/mason.nvim" },
-        { "williamboman/mason-lspconfig.nvim" },
-      },
-      config = function()
-        local lsp_zero = require("lsp-zero")
-
-        -- lsp_attach is where you enable features that only work
-        -- if there is a language server active in the file
-        local lsp_attach = function(client, bufnr)
-          local opts = { buffer = bufnr }
-
-          vim.keymap.set("n", "K", "<cmd>lua vim.lsp.buf.hover()<cr>", opts)
-          vim.keymap.set("n", "gd", "<cmd>lua vim.lsp.buf.definition()<cr>", opts)
-          vim.keymap.set("n", "gD", "<cmd>lua vim.lsp.buf.declaration()<cr>", opts)
-          vim.keymap.set("n", "gi", "<cmd>lua vim.lsp.buf.implementation()<cr>", opts)
-          vim.keymap.set("n", "go", "<cmd>lua vim.lsp.buf.type_definition()<cr>", opts)
-          vim.keymap.set("n", "gr", "<cmd>lua vim.lsp.buf.references()<cr>", opts)
-          vim.keymap.set("n", "gs", "<cmd>lua vim.lsp.buf.signature_help()<cr>", opts)
-          vim.keymap.set("n", "<F2>", "<cmd>lua vim.lsp.buf.rename()<cr>", opts)
-          vim.keymap.set({ "n", "x" }, "<F3>", "<cmd>lua vim.lsp.buf.format({async = true})<cr>", opts)
-          vim.keymap.set("n", "<F4>", "<cmd>lua vim.lsp.buf.code_action()<cr>", opts)
-        end
-
-        lsp_zero.extend_lspconfig({
-          sign_text = true,
-          lsp_attach = lsp_attach,
-          capabilities = require("cmp_nvim_lsp").default_capabilities(),
-        })
-
-        require("mason-lspconfig").setup({
-          ensure_installed = {},
-          handlers = {
-            -- this first function is the "default handler"
-            -- it applies to every language server without a "custom handler"
-            function()
-              require("lspconfig").lua_ls.setup({})
-              require("lspconfig").asm_lsp.setup({})
-              require("lspconfig").ast_grep.setup({})
-              require("lspconfig").hls.setup({})
-              require("lspconfig").texlab.setup({})
-              require("lspconfig").matlab_ls.setup({})
-              require("lspconfig").r_language_server.setup({})
-              require("lspconfig").zls.setup({})
-            end,
-          },
-        })
-      end,
-    },
-  },
+			require("mason-lspconfig").setup({
+				ensure_installed = {},
+				handlers = {
+					function()
+						require("lspconfig").lua_ls.setup({})
+						require("lspconfig").asm_lsp.setup({})
+						require("lspconfig").ast_grep.setup({})
+						require("lspconfig").hls.setup({})
+						require("lspconfig").texlab.setup({})
+						require("lspconfig").matlab_ls.setup({})
+						require("lspconfig").r_language_server.setup({})
+						require("lspconfig").zls.setup({})
+					end,
+				},
+			})
+		end,
+	},
+	{
+		"mhartington/formatter.nvim",
+		config = function()
+			require("formatter").setup({
+				logging = false,
+				filetype = {
+					sh = {
+						function()
+							return {
+								exe = "shfmt",
+								args = { "-w" },
+								stdin = true,
+							}
+						end,
+					},
+					c = {
+						function()
+							return {
+								exe = "clang-format",
+								args = { "--style=google" },
+								stdin = true,
+							}
+						end,
+					},
+					matlab = {
+						function()
+							return {
+								exe = "mlint",
+								args = {},
+								stdin = false,
+							}
+						end,
+					},
+					tex = {
+						function()
+							return {
+								exe = "latexindent",
+								args = { "-y", '{"indent": "    "}' },
+								stdin = true,
+							}
+						end,
+					},
+					python = {
+						function()
+							return {
+								exe = "black",
+								args = { "--fast" },
+								stdin = true,
+							}
+						end,
+					},
+				},
+			})
+		end,
+	},
 }
 
-local opts = {}
-
-require("lazy").setup(plugins, opts)
+require("lazy").setup(plugins)
 
 local builtin = require("telescope.builtin")
 vim.keymap.set("n", "<C-p>", builtin.find_files, {})
@@ -159,19 +201,13 @@ vim.keymap.set("n", "<M-p>", builtin.live_grep, {})
 
 local config = require("nvim-treesitter.configs")
 config.setup({
-  ensure_installed = { "lua", "c", "go", "haskell", "latex", "python", "r", "zig", "asm" },
-  highlight = { enable = true },
-  indent = { enable = true },
-  sync_install = false,
+	ensure_installed = { "lua", "c", "go", "haskell", "latex", "python", "r", "zig", "asm" },
+	highlight = { enable = true },
+	indent = { enable = true },
+	sync_install = false,
 })
 
 vim.keymap.set("n", "<C-n>", ":Neotree filesystem reveal left<CR>")
 
 require("catppuccin").setup()
 vim.cmd.colorscheme("catppuccin")
-
-require("lualine").setup({
-  otions = {
-    theme = "dracula",
-  },
-})
